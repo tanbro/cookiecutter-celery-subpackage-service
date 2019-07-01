15a52970f8446473b0ec4efeabec263e09cfe9f0
liu xue yan <liu_xue_yan@foxmail.com>
2019-07-01 10:48:44 +0800

    add: setuptools version check

--- {{cookiecutter.project_dir}}/setup.py
+++ {{cookiecutter.project_dir}}/setup.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+import setuptools
+from distutils.version import StrictVersion
 from setuptools import setup
+
+
+def check_setuptools_version():
+    actual_ver = StrictVersion(setuptools.__version__)
+    requires_min_ver = StrictVersion('40.1.0')
+
+    if actual_ver < requires_min_ver:
+        msg = \
+            'Expected version of setuptools is greater than or equals to {0}, but actural is {1}.'.format(requires_min_ver, actual_ver) + os.linesep + \
+            'Upgrade setuptools by: ' + os.linesep + \
+            '\tpip install -U setuptools'
+        print(msg, file=sys.stderr)
+        sys.exit(1)
+
+
+check_setuptools_version()
 
 setup()
